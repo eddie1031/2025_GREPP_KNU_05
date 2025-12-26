82c57aac45a3b234a2a366dd2784be15351e9020
eddie1031 <archaco123@gmail.com>
2025-12-26 11:31:02 +0900

    Tasklet Job

--- src/main/java/io/eddie/batchexp/C01/job/LoggingTaskletJobConfiguration.java
+++ src/main/java/io/eddie/batchexp/C01/job/LoggingTaskletJobConfiguration.java
@@ -1,26 +1,31 @@
 package io.eddie.batchexp.C01.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
 public class LoggingTaskletJobConfiguration {
 
     @Bean
     public Job loggingJob(
             JobRepository jobRepository,
-            @Qualifier("printHelloStep") Step printHelloStep
+            @Qualifier("printHelloStep") Step printHelloStep,
+            @Qualifier("printWorldStep") Step printWorldStep
     ) {
         return new JobBuilder("loggingJob", jobRepository)
                 .start(printHelloStep)
+                .next(printWorldStep)
                 .build();
     }
 
@@ -30,14 +35,33 @@ public class LoggingTaskletJobConfiguration {
             PlatformTransactionManager platformTransactionManager
     ) {
         return new StepBuilder("printHelloStep", jobRepository)
-                .tasklet(, platformTransactionManager)
+                .tasklet(printHelloTasklet(), platformTransactionManager)
                 .build();
     }
 
-    @Bean
+    @Bean(name = "printHelloTasklet")
     public Tasklet printHelloTasklet() {
         return (contribution, chunkContext) -> {
+            log.info("Hello, ");
+            return RepeatStatus.FINISHED;
+        };
+    }
 
+    @Bean(name = "printWorldStep")
+    public Step printWorldStep(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager
+    ) {
+        return new StepBuilder("printWorldStep", jobRepository)
+                .tasklet(printWorldTasklet(), platformTransactionManager)
+                .build();
+    }
+
+    @Bean(name = "printWorldTasklet")
+    public Tasklet printWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            log.info("World!");
+            return RepeatStatus.FINISHED;
         };
     }
 
